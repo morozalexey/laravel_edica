459af2eb12f55da81abcce9b8db0c8d4a8b12d04
Alexey Moroz <15webmoroz@gmail.com>
2022-12-10 14:46:45 +0300

    Сделал job для регистрации пользователей, поправил хлебные крошки, русифицировал формы входа и регистрации

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Подтвердите свой email') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Ссылка для верификации была выслана на Вашу почту') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Прежде чем продолжить, пожалуйста проверьте свою почту и перейдите по ссылке для верификации Вашей учетной записи.') }}
+                    {{ __('Если вы не получили письмо') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Нажмите тут, чтобы мы прислали Вам новое') }}</button>.
                     </form>
                 </div>
             </div>
